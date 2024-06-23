e278b166bf1840bbb007b76c0d5bfbb2b1ae45cc
Luffier <Luffier@users.noreply.github.com>
2024-06-23 17:21:44 +0100

    Bump script version

--- ladderr.js
+++ ladderr.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Ladderr
-// @version      0.5.4
+// @version      0.5.5
 // @description  Access your remote files directly from qBittorrent Web UI, just like in the desktop app.
 // @author       luffier
 // @namespace    ladderr
